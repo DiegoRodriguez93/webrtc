0a8da4cd1c787d6056d4a405eb696eaed2bb8f90
diegorodriguezvida <d.rodriguez@vida.com.uy>
2020-08-24 10:50:13 -0300

    cambio de puerto al 12999

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-let express = require( 'express' );
+let express = require('express');
 let app = express();
-let server = require( 'http' ).Server( app );
-let path = require( 'path' );
+let server = require('http').Server(app);
+let path = require('path');
 
-let io = require( 'socket.io' )( server );
-let stream = require( './ws/stream' );
+let io = require('socket.io')(server);
+let stream = require('./ws/stream');
 
-let favicon = require( 'serve-favicon' );
+let favicon = require('serve-favicon');
 
-app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
-app.use( '/components', express.static( path.join( __dirname, 'components' ) ) );
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use('/components', express.static(path.join(__dirname, 'components')));
 
-app.get( '/', ( req, res ) => {
-    res.sendFile( __dirname + '/index.html' );
-} );
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
-io.of( '/stream' ).on( 'connection', stream );
+io.of('/stream').on('connection', stream);
 
-server.listen( 3000 );
+server.listen(3999);
 
 
 
